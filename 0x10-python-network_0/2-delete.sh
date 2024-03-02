2b6a5331e9d19518d9888eb31a53fa1e0c6c1aff
philanthropist aria <136027939+philanthropistaria@users.noreply.github.com>
2024-03-02 13:04:19 +0000

    Update 2-delete.sh

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-## Send a DELETE request to a given URL and display the body of the response.
-#curl -sX DELETE "$1"
+# Send a DELETE request to a given URL and display the body of the response.
+curl -sX DELETE "$1"
